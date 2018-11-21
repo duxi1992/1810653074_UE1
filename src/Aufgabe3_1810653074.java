1f3e4a5b2aefb80a6ca3b6569c90e251b4b7714e
Riccardo Duxenneuner <riccardo@duxi.at>
2018-11-21 08:45:28 +0100

    Aufgabe3

---
+++ src/Aufgabe3_1810653074.java
@@ -0,0 +1,26 @@
+public class Aufgabe3_1810653074 {
+
+    public static void main(String[] args) {
+        int zahl1 = 4;
+        int zahl2 = 5;
+        int zahl3 = 15;
+        double zahl4 = 25;
+        double zahl5 = -25;
+        double zahl6 = -23;
+
+        int addition = zahl1 + zahl2;
+        System.out.println(addition);
+
+        int division = zahl1 / zahl3;
+        System.out.println(division);
+
+        int modulo = zahl1 % zahl2;
+        System.out.println(modulo);
+
+        int inkrementieren = zahl3++;
+        System.out.println(inkrementieren);
+
+        int dekrementieren = zahl2--;
+        System.out.println(dekrementieren);
+    }
+}
---
+++ src/Aufgabe4_1810653074.java
@@ -0,0 +1,4 @@
+package PACKAGE_NAME;
+
+public class Aufgabe4_1810653074 {
+}
